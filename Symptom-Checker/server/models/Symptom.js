edd9700c382b1c028a48fb9cac506eee7ec74594
Gowri <gowrid.23it@kongu.edu>
2025-06-23 15:46:34 +0530

    day6

--- Symptom-Checker/server/models/Symptom.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const symptomSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  severity: String
-});
-
-module.exports = mongoose.model('Symptom', symptomSchema);
