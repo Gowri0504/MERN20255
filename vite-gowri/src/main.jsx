8274144e5134783a0b3c559b12e6e9bc34bc5208
Gowri <gowrid.23it@kongu.edu>
2025-06-25 16:02:16 +0530

    completed

--- vite-gowri/src/main.jsx
+++ vite-gowri/src/main.jsx
@@ -4,13 +4,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "./index.css"
-import { UserContext } from './hooks/Context.jsx'
+// import { UserContext } from './hooks/Context.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <UserContext>
+    {/* <UserContext> */}
       <App />
-    </UserContext>
+    {/* </UserContext> */}
     </BrowserRouter>
 
   </StrictMode>,
